49d565d24e445390a8d1e1916e866e3ded89c44b
Schalk Burger <schalkb@gmail.com>
2022-12-10 22:09:01 +0200

    Update Steam Mass Comments Poster

--- steam-profile-artwork.user.js
+++ steam-profile-artwork.user.js
@@ -4,10 +4,10 @@
 // @match       https://steamcommunity.com/sharedfiles/edititem/767/3/
 // @match       https://steamcommunity.com/id/*/edit/info
 // @match       https://steamcommunity.com/id/*
+// @match       https://steamcommunity.com/id/*/friends/
 // @include     /^https?:\/\/steamcommunity.com\/(id\/+[A-Za-z0-9$-_.+!*'(),]+|profiles\/7656119[0-9]{10})\/friends\/?$/
-// @exclude     https://steamcommunity.com/id/*/*/
 // @exclude     https://steamcommunity.com/id/*/inventory/*
-// @version     1.3.5
+// @version     1.3.6
 // @author      Schalk Burger <schalkb@gmail.com>
 // @description  A tool to make it easier to upload custom artwork for your profile.
 // @license MIT
@@ -21,7 +21,7 @@
 
 (function () {
   "use strict";
-  console.log("Steam Artwork Tool Version 1.3.5")
+  console.log("Steam Artwork Tool Version 1.3.6")
   // Inject Steam Profile Artwork Tool styles
   let css = `
   .steamProfileArtworkContainer {
@@ -221,6 +221,9 @@
   }
   #manage_friends .friends-comments-textarea {
     width: 100%;
+  }
+  #manage_friends.manage_friends_panel.manage {
+    padding-bottom: 20px;
   }
   `,
     head = document.head || document.getElementsByTagName("head")[0],
@@ -884,7 +887,6 @@
   const manageFriendsSelector = document.querySelector("#manage_friends > .row");
   const manageFriendsSelectorParent = document.querySelector("#manage_friends");
 
-
   const manageFriendsComments = document.createElement("div");
   manageFriendsComments.className = "friends-comments-textarea";
   manageFriendsComments.innerHTML = `<div class="row commentthread_entry" style="background-color: initial; padding-right: 24px;">
@@ -915,64 +917,38 @@
 
   manageFriendsSelectorParent.insertBefore(manageFriendsComments, manageFriendsSelector);
 
+  const commentSubmitButton = document.querySelector("#comment_submit");
+  const commentTextarea = document.querySelector("#comment_textarea");
+  const commentLogHead = document.querySelector("#log_head");
+  const commentLogBody = document.querySelector("#log_body");
+  commentSubmitButton.addEventListener("click", (e) => {
+    // e.preventDefault();
+    const selectedCheckbox = document.querySelector(".selected");
+    const totalSelected = selectedCheckbox?.length;
+    const commentMessage = commentTextarea.value;
+    if (totalSelected === 0 || commentMessage.length === 0) {
+      alert("Please make sure you entered a message and selected 1 or more friends.");
+      return;
+    }
+
+    commentLogHead.innerHTML = "";
+    commentLogBody.innerHTML = "";
+
+    document.querySelector(".selected").each((i, elem) => {
+      let profileID = document.querySelector(elem).data("steamid");
+      setTimeout(() => $.post("//steamcommunity.com/comment/Profile/post/" + profileID + "/-1/", {
+        comment: msg,
+        count: 6,
+        sessionid: g_sessionID
+      }, response => {
+        document.querySelector("#log_body").get()[0].innerHTML += "<br>" + (response.success === false ? response.error : "Successfully posted comment on <a href="https://steamcommunity.com/profiles/" + profileID + "/#commentthread_Profile_" + profileID + "_0_area">" + profileID + "</a>");
+        document.querySelector(".friend_block_v2[data-steamid=" + profileID + "]").classList.remove("selected").querySelector(".select_friend_checkbox").prop("checked", false);
+        UpdateSelection();
+      })
+        .fail(() => document.querySelector("#log_body").get()[0].innerHTML += "<br>Failed to post comment on <a href="http://steamcommunity.com/profiles/" + profileID + "/">" + profileID + "</a>")
+          .always(() => document.querySelector("#log_head").html("<br><b>Processed " + (i + 1) + " out of " + total + " friend" + (total.length === 1 ? "" : "s") + ".<b>")), delay * i * 1000);
+    });
+
+  });
+
 })();
-
-// Steam Mass Comments Poster
-
-// ==Configuration==
-// const delay = 7; // Seconds in between posting profile comments
-// ==/Configuration==
-
-// ==Code==
-// ToggleManageFriends();
-// jQuery(function ($) {
-//   $("#manage_friends > .row:last").before(`
-//     <div class="row commentthread_entry" style="background-color: initial; padding-right: 24px;">
-//         <div class="commentthread_entry_quotebox">
-//             <textarea rows="3" class="commentthread_textarea" id="comment_textarea" placeholder="Add a comment" style="overflow: hidden; height: 20px;"></textarea>
-//         </div>
-//         <div class="commentthread_entry_submitlink" style="">
-//             <a class="btn_grey_black btn_small_thin" href="javascript:CCommentThread.FormattingHelpPopup('Profile');">
-//             <span>Formatting help</span>
-//             </a>
-//             <span class="emoticon_container">
-//             <span class="emoticon_button small" id="emoticonbtn">
-//             </span>
-//             </span>
-//             <span class="btn_green_white_innerfade btn_small" id="comment_submit">
-//             <span>Post Comments to Selected Friends</span>
-//             </span>
-//         </div>
-//     </div>
-//     <div class="row" id="log">
-//         <span id="log_head"></span>
-//         <span id="log_body"></span>
-//     </div>`);
-
-//   new CEmoticonPopup($J('#emoticonbtn'), $J('#commentthread_Profile_0_textarea'));
-//   $("#comment_submit").click(() => {
-//     const total = $(".selected").length;
-//     const msg = $("#comment_textarea").val();
-//     if (total === 0 || msg.length === 0) {
-//       alert("Please make sure you entered a message and selected 1 or more friends.");
-//       return;
-//     }
-
-//     $("#log_head, #log_body").html("");
-//     $(".selected").each((i, elem) => {
-//       let profileID = $(elem).data("steamid");
-//       setTimeout(() => $.post("//steamcommunity.com/comment/Profile/post/" + profileID + "/-1/", {
-//         comment: msg,
-//         count: 6,
-//         sessionid: g_sessionID
-//       }, response => {
-//         $("#log_body").get()[0].innerHTML += "<br>" + (response.success === false ? response.error : "Successfully posted comment on <a href=\"https://steamcommunity.com/profiles/" + profileID + "/#commentthread_Profile_" + profileID + "_0_area\">" + profileID + "</a>");
-//         $(".friend_block_v2[data-steamid=" + profileID + "]").removeClass("selected").find(".select_friend_checkbox").prop("checked", false);
-//         UpdateSelection();
-//       })
-//         .fail(() => $("#log_body").get()[0].innerHTML += "<br>Failed to post comment on <a href=\"http://steamcommunity.com/profiles/" + profileID + "/\">" + profileID + "</a>")
-//         .always(() => $("#log_head").html("<br><b>Processed " + (i + 1) + " out of " + total + " friend" + (total.length === 1 ? "" : "s") + ".<b>")), delay * i * 1000);
-//     });
-
-//   });
-// });
